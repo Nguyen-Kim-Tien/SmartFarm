9ac1958527f18447766c119c702ff0a433bf05c1
Nguyen-Kim-Tien <tien.nguyenctcxht11@hcmut.edu.vn>
2023-03-04 04:41:28 +0700

    Complete TodoList

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import React from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, ScrollView, Alert } from "react-native";
 import Task from "./components/Task";
 import styles from "./App.components.style";
 import Form from "./components/Form";
 
 export default function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  const handleAddTASK = (task) => {
+    setTaskList([...taskList, task]);
+  };
+  const handleDeleteTask = (index) => {
+    Alert.alert("Thông báo !!!", "Bạn có chắc chắn muốn xóa ?", [
+      {
+        text: "OK",
+        onPress: () => {
+          let taskListTmp = [...taskList];
+          taskListTmp.splice(index, 1);
+          setTaskList(taskListTmp);
+        },
+      },
+      { text: "cancel", onPress: () => {} },
+    ]);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.body}>
         <Text style={styles.header}>Todo List</Text>
         <ScrollView style={styles.items}>
-          <Task />
-          <Task />
-          <Task />
+          {taskList.map((item, index) => {
+            return (
+              <Task
+                key={index}
+                title={item}
+                number={index + 1}
+                onDeleteTask={() => handleDeleteTask(index)}
+              />
+            );
+          })}
         </ScrollView>
       </View>
-      <Form />
+      <Form onAddTASK={handleAddTASK} />
     </View>
   );
 }
